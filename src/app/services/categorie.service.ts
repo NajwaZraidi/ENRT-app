5e4571766ab348870eeaf1c69c934a3dbcf571d4
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2024-03-05 09:48:35 +0100

    change api endpoints

--- src/app/services/categorie.service.ts
+++ src/app/services/categorie.service.ts
@@ -18,6 +18,7 @@ export class CategorieService {
   }
   GetCategorieByID(id:string): Observable<any>{
     return this.http.get('http://localhost:8080/categories/by-id/'+id);
+
   }
 
   DeleteCategorie(id :string){
